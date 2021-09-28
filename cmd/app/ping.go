7b1138396fe78c26548ec0ea06809a534fbbc462
Para0234 <Para@risistar.fr>
2021-09-28 20:14:21 +0200

    Added process.env

--- cmd/app/ping.go
+++ cmd/app/ping.go
@@ -23,8 +23,8 @@ func init() {
 
 func bot() {
 
-	// Create a new Discord session using the provided bot token. Currently using Carlos Bot's token.
-	dg, err := discordgo.New("Bot " + "Token")
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
