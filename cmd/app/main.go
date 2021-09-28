8f1ea907c4fa09b07033407e89ba78009b7b9b06
Jean-Baptiste Agostin <jean-baptiste.agostin@abtasty.com>
2021-09-28 16:22:31 +0200

    Add: base folder project

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"time"
-
 	"github.com/HETIC-MT-P2021/PROJECT_FINAL_GROUP05/pkg/rabbit"
+	"log"
 )
 
 func main() {
@@ -15,22 +12,7 @@ func main() {
 		return
 	}
 
-	rabbitImpl := rabbit.NewRabbitRepository(rabbit.RabbitChannel, rabbit.RabbitQueue)
-	
-	message := rabbit.QueueMessage{
-		Type: "audio",
-		MediaLink: "youtube.com",
-		Options: 	rabbit.Options{
-			StartInSeconds: "10",
-			DurationInSeconds: "10",
-		},
-	}
-	buffer, err := json.Marshal(message)
-	if err != nil {
-		return
-	}
+	_ = rabbit.NewRabbitRepository(rabbit.RabbitChannel, rabbit.RabbitQueue)
 
-  time.Sleep(10 * time.Second) // 10s to be sure rabbit is ready
-
-	rabbitImpl.Publish(string(buffer))
+	log.Println("⚡️ app is running!")
 }
