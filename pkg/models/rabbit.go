042928e01ea22a6ac41a06dacfe9421cbf850ff3
Jibe0123 <Jbagostin@gmail.com>
2021-11-04 19:06:54 +0100

    Feat: add options array for filters for each medias

--- pkg/models/rabbit.go
+++ pkg/models/rabbit.go
@@ -3,32 +3,33 @@ package models
 import "github.com/bwmarrin/discordgo"
 
 type DownloadQueueMessage struct {
-	Type      			string  								`json:"type"`
-	MediaLink 			string  								`json:"media_link"`
-	Options   			Options 								`json:"options"`
-	DiscordInfo 		JobCheckerQueueMessage 	`json:"discord_info"`
+	Type        string                 `json:"type"`
+	MediaLink   string                 `json:"media_link"`
+	Options     Options                `json:"options"`
+	DiscordInfo JobCheckerQueueMessage `json:"discord_info"`
 }
 
 type MediaProcessingQueueMessage struct {
-	Type      			string  								`json:"type"`
-	FileName 				string  								`json:"file_name"`
-	Options  				Options 								`json:"options"`
-	DiscordInfo 		JobCheckerQueueMessage 	`json:"discord_info"`
+	Type        string                 `json:"type"`
+	FileName    string                 `json:"file_name"`
+	Options     Options                `json:"options"`
+	DiscordInfo JobCheckerQueueMessage `json:"discord_info"`
 }
 
 type JobCheckerQueueMessage struct {
-	ServerID					string					`json:"server_id"`
-	ChannelID 				string  				`json:"channel_id"`
-	MessageID  				string 					`json:"message_id"`
+	ServerID  string `json:"server_id"`
+	ChannelID string `json:"channel_id"`
+	MessageID string `json:"message_id"`
 }
 
 type Options struct {
-	StartInSeconds    int `json:"start"`
-	DurationInSeconds int `json:"duration"`
+	StartInSeconds    int      `json:"start"`
+	DurationInSeconds int      `json:"duration"`
+	Filters           []string `json:"filters"`
 }
 
 type DiscordSession struct {
-	Session 		*discordgo.Session	`json:"session"`
-	ChannelID 	string 							`json:"channel_id"`
-	MessageID 	string 							`json:"message"`
+	Session   *discordgo.Session `json:"session"`
+	ChannelID string             `json:"channel_id"`
+	MessageID string             `json:"message"`
 }
